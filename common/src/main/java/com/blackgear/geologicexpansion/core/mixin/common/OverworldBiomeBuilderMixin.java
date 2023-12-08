888179ddbdcf288910903acc2754930eefd5c663
ItsBlackGear <itsblackgear@gmail.com>
2023-12-08 16:55:32 -0300

    🚀 1.0 Release
    updated biome placement

--- common/src/main/java/com/blackgear/geologicexpansion/core/mixin/common/OverworldBiomeBuilderMixin.java
+++ common/src/main/java/com/blackgear/geologicexpansion/core/mixin/common/OverworldBiomeBuilderMixin.java
@@ -1,67 +1,21 @@
 package com.blackgear.geologicexpansion.core.mixin.common;
 
 import com.blackgear.geologicexpansion.common.registries.worldgen.GEBiomes;
-import com.mojang.datafixers.util.Pair;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Climate;
 import net.minecraft.world.level.biome.OverworldBiomeBuilder;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.function.Consumer;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(OverworldBiomeBuilder.class)
 public abstract class OverworldBiomeBuilderMixin {
-    @Shadow protected abstract void addSurfaceBiome(Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> consumer, Climate.Parameter temperature, Climate.Parameter humidity, Climate.Parameter continentalness, Climate.Parameter erosion, Climate.Parameter depth, float weirdness, ResourceKey<Biome> key);
-
-    @Shadow @Final private Climate.Parameter[] temperatures;
-
-    @Shadow @Final private Climate.Parameter[] humidities;
-
-    @Shadow @Final private Climate.Parameter midInlandContinentalness;
-
-    @Shadow @Final private Climate.Parameter[] erosions;
-
-    @Inject(method = "addValleys", at = @At("TAIL"))
-    private void ge$addValleys(Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> consumer, Climate.Parameter depth, CallbackInfo ci) {
-        generatePrismaticCaldera(consumer, depth);
-    }
-
-    @Inject(method = "addLowSlice", at = @At("TAIL"))
-    private void ge$addLowSlice(Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> consumer, Climate.Parameter depth, CallbackInfo ci) {
-        generatePrismaticCaldera(consumer, depth);
-    }
-
-    @Inject(method = "addMidSlice", at = @At("TAIL"))
-    private void ge$addMidSlice(Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> consumer, Climate.Parameter depth, CallbackInfo ci) {
-        generatePrismaticCaldera(consumer, depth);
-    }
-
-    @Inject(method = "addHighSlice", at = @At("TAIL"))
-    private void ge$addHighSlice(Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> consumer, Climate.Parameter depth, CallbackInfo ci) {
-        generatePrismaticCaldera(consumer, depth);
-    }
-
-    @Inject(method = "addPeaks", at = @At("TAIL"))
-    private void ge$addPeaks(Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> consumer, Climate.Parameter depth, CallbackInfo ci) {
-        generatePrismaticCaldera(consumer, depth);
-    }
-
-    @Unique
-    private void generatePrismaticCaldera(Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> consumer, Climate.Parameter depth) {
-        this.addSurfaceBiome(consumer,
-                this.temperatures[2],
-                this.humidities[3],
-                this.midInlandContinentalness,
-                Climate.Parameter.span(this.erosions[2], this.erosions[3]), depth,
-                0,
-                GEBiomes.PRISMATIC_CALDERA
-        );
+    @Inject(method = "pickMiddleBiome", at = @At("HEAD"), cancellable = true)
+    private void ge$pickMiddleBiome(int temperature, int humidity, Climate.Parameter param, CallbackInfoReturnable<ResourceKey<Biome>> cir) {
+        if (param.max() >= 0L && temperature == 1 && humidity == 3) {
+            cir.setReturnValue(GEBiomes.PRISMATIC_CALDERA);
+        }
     }
 }
